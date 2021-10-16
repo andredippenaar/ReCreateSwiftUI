0bba8b1745f4744dcafff81da677491eaf0c15d6
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-10-16 09:56:52 +0200

    Initial Commit

---
+++ ReCreateSwiftUI/ReCreateSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReCreateSwiftUIApp.swift
+//  ReCreateSwiftUI
+//
+//  Created by Andre Dippenaar on 2021/10/16.
+//
+
+import SwiftUI
+
+@main
+struct ReCreateSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
